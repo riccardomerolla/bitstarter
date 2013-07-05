d98bef4a9b208e2d866be53a4ca97067e18f75f1
riccardo.merolla <riccardo.merolla@gmail.com>
2013-07-05 07:10:39 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var infile = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('Hello World 2!');
+    buf = fs.readFileSync(infile);
+    response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
